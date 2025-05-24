af19d2cae0a145367bafb2c44747e4b1b49abb39
Reza-namvaran <reza.namvaran823@gmail.com>
2025-05-25 00:52:54 +0330

    (feat): Add step method

--- turing/machine.py
+++ turing/machine.py
@@ -1,3 +1,5 @@
+from .exceptions import SimulationError
+
 class TuringMachine: 
     def __init__(self, config : dict):
         self.number_of_tape = config['tapes']
@@ -13,4 +15,35 @@ class TuringMachine:
         self.halt = False
         self.step_count = 0
 
-        def initialize_tape() -> None: pass 
+        def initialize_tape(self, inputs:  list[str]) -> None: 
+            """Initialize tapes with input strings"""
+            self.tapes = []
+            for i in range(self.number_of_tape):
+                input_str = inputs[i] if i < len(inputs) else ''
+                self.tapes.append(list(input_str.strip()))
+        
+        def step(self) -> None:
+            if self.halt:
+                raise SimulationError("Machine Already Halted!")
+
+            current_symbols = self._read_symbols()
+            transition = self._find_transition(current_symbols)
+
+            if not transition:
+                self.halt = True
+                return
+            
+            self._apply_transition(transition)
+            self.step_count += 1
+
+            if self.current_state in self.final_states:
+                self.halted = True
+
+        def _read_symbol():
+            pass
+
+        def _apply_transition(self, transition):
+            pass
+
+        def _find_transition(self, symbols):
+            pass
