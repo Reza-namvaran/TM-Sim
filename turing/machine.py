17b3b614f6e46570ef76104f7dced8dbdf8c2582
Reza-namvaran <reza.namvaran823@gmail.com>
2025-05-31 21:10:05 +0330

    (refactor): Change apply transition logic
    Fix out of range error for head

--- turing/machine.py
+++ turing/machine.py
@@ -8,13 +8,16 @@ class TuringMachine:
         self.number_of_tapes = config['tapes']
         self.blank_symbol = config['blank_symbol']
         self.transitions = config['transition']
-        self.iniial_state = config['initial_state']
-        self.final_states = config.get('final_state', [])
+        self.initial_state = config['initial_state']
+
+        self.final_states = config.get('final_states', [])
+        if not self.final_states and 'final_state' in config:
+            self.final_states = [config['final_state']]
 
         # Initialize tapes and heads
         self.tapes = []
         self.heads = [0] * self.number_of_tapes
-        self.current_state = self.iniial_state
+        self.current_state = self.initial_state
         self.halt = False
         self.step_count = 0
 
@@ -57,9 +60,9 @@ class TuringMachine:
             self.halt = True
             self.logger.info(f"Reached final state: {self.current_state}")
 
-    def _read_symbol(self) -> list:
+    def _read_symbols(self) -> list:
         symbols = []
-        for i in range(self.num_tapes):
+        for i in range(self.number_of_tapes):
             if self.heads[i] < len(self.tapes[i]):
                 symbols.append(self.tapes[i][self.heads[i]])
             else:
@@ -72,20 +75,25 @@ class TuringMachine:
                 trans[0][1] == symbols):
                 return trans
         return None
-    
+   
     def _apply_transition(self, transition) -> None:
-            new_state, write_symbols, moves = transition[1]
-            self.current_state = new_state
+        new_state, write_symbols, moves = transition[1]
+        self.current_state = new_state
 
-            for i in range(self.number_of_tapes):
-                # Ensure tape has enough cells
-                while self.heads[i] >= len(self.tapes[i]):
-                    self.tapes[i].append(self.blank_symbol)
-                    
-                # Write symbol
-                self.tapes[i][self.heads[i]] = write_symbols[i]
-                if moves[i] == 'R':
-                    self.heads[i] += 1
-                elif moves[i] == 'L':
-                    self.heads[i] -= max(0, self.heads[i] - 1)
+        for i in range(self.number_of_tapes):
+            # Ensure tape has enough cells BEFORE writing
+            while self.heads[i] >= len(self.tapes[i]):
+                self.tapes[i].append(self.blank_symbol)
                 
+            # Write symbol
+            self.tapes[i][self.heads[i]] = write_symbols[i]
+            
+            # Move head
+            if moves[i] == 'R':
+                self.heads[i] += 1
+            elif moves[i] == 'L':
+                self.heads[i] = max(0, self.heads[i] - 1)
+                
+            # Ensure tape has enough cells AFTER moving
+            while self.heads[i] >= len(self.tapes[i]):
+                self.tapes[i].append(self.blank_symbol)
